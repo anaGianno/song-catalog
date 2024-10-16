19413f801657923e749252ca3e95e7857a9f4840
Coronado <bc245@lab-rg19-12.cms.waikato.ac.nz>
2024-10-17 11:49:25 +1300

    load xml file to display tracks  and add style to index.php

--- details.php
+++ details.php
@@ -6,18 +6,21 @@
         <!-- import font -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href = "style.css">
     </head>
     <body>
         <div class = "header"></div>
         <div class = "content">
-
+            <?php
+                $title = $_POST["title"];
+                
+                echo "<p>".$title."</p>";
+                echo "<p>test</p>";
+            ?>
         </div>
-        <?php
-            
-        ?>
+
 
     </body>
 </html>
--- index.php
+++ index.php
@@ -6,54 +6,41 @@
         <!-- import font -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href = "style.css">
     </head>
     <body>
         <div class = "header">
-            <!-- <p class = "header-text">song catalog</p> -->
+            <p class = "header-text">Song Catalog</p>
         </div>
         <div class = "content">
             <form method='POST' name='form1'  action='details.php' autocomplete='off'>
                 <div class = "grid-track">
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
-                    <div class = "track"></div>
+                    <?php
+                        if(file_exists('catalog.xml')){
+                            $tracks = simplexml_load_file('catalog.xml');
+                            // var_dump($tracks);
+                        }
+                        else{
+                            exit('Failed to open test.xml');
+                        }
+
+                        foreach($tracks->children() as $track){
+                            $att = $track->attributes();
+                            echo "<input class = 'track' type='submit' name='title' value='".$att['title']."'></input>\n";
+                        }
+                    ?>
                 </div>
-                <input class ='buttonindex' type='submit' value='SUBMIT FORM'> 
-                <select name="sort" id="sort">
+            
+                <select class ="sort" name="sort" id="sort">
                     <option value="1">Default</option>
                     <option value="2">Alphabetical</option>
                 </select>
             </form>
         </div>
 
-        <?php
 
-        ?>
 
     </body>
 </html>
