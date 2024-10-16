386b758f68d6de3a478a4bc6c10c66c64ba29ba3
Coronado <bc245@lab-rg19-12.cms.waikato.ac.nz>
2024-10-17 12:37:53 +1300

    Display details in details.php using xpath

--- details.php
+++ details.php
@@ -11,13 +11,25 @@
         <link rel="stylesheet" href = "style.css">
     </head>
     <body>
-        <div class = "header"></div>
+        <div class = "header">
+            <p class = "header-text">Song Catalog</p>
+        </div>
         <div class = "content">
             <?php
                 $title = $_POST["title"];
-                
-                echo "<p>".$title."</p>";
-                echo "<p>test</p>";
+
+                $tracks = simplexml_load_file("catalog.xml");
+                $selectedTrack = $tracks->xpath("//track[@title='".$title."']");
+                foreach($selectedTrack as $track){
+                    $att = $track->attributes();
+                    echo "<div>".$att['title']."</div><br>";
+                    echo "<div>".$track->from . "</div><br>";
+                    echo "<div>".$track->by. "</div><br>";
+                    echo "<div>".$track->release_date. "</div><br>";
+                    echo "<div>".$track->length. "</div><br>";
+                    echo "<div>".$track->genre. "</div><br>";
+                }
+
             ?>
         </div>
 
